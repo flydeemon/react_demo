01ba26dc2835de0beed6de2fd08755402ac48b75
deemon <792229765@qq.com>
2017-04-18 20:04:20 +0800

    上传项目

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,29 @@
-'use strict';
 
-const path = require('path');
-const args = require('minimist')(process.argv.slice(2));
+var path = require('path');
+var webpack = require('webpack');
 
-// List of allowed environments
-const allowedEnvs = ['dev', 'dist', 'test'];
-
-// Set the correct environment
-let env;
-if (args._.length > 0 && args._.indexOf('start') !== -1) {
-  env = 'test';
-} else if (args.env) {
-  env = args.env;
-} else {
-  env = 'dev';
-}
-process.env.REACT_WEBPACK_ENV = env;
-
-/**
- * Build the webpack configuration
- * @param  {String} wantedEnv The wanted environment
- * @return {Object} Webpack config
- */
-function buildConfig(wantedEnv) {
-  let isValid = wantedEnv && wantedEnv.length > 0 && allowedEnvs.indexOf(wantedEnv) !== -1;
-  let validEnv = isValid ? wantedEnv : 'dev';
-  let config = require(path.join(__dirname, 'cfg/' + validEnv));
-  return config;
-}
-
-module.exports = buildConfig(env);
+module.exports = {
+    context: __dirname + '/src',
+    entry: './main.js', // 入口文件，单入口 app.jsx 文件
+    output: {
+        path: __dirname + '/assets/',
+        filename: 'bundle.js',
+        publicPath: '/assets/'
+    }, 
+    devtool: 'source-map', // 调试 es6 代码
+    module: {
+        loaders: [ // 使用特定的加载器 loader 处理特定的文件
+            {
+                test: /\.js?$/, // 文件过滤规则
+                loader: 'babel-loader',
+                exclude: /(node_modules)/,
+                query: {
+                    presets: ['es2015', 'react'] // es2015 处理 ES6 语法，react 处理 jsx 语法
+                }
+            }, {
+                test: /\.css?$/, // css
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    }
+};
